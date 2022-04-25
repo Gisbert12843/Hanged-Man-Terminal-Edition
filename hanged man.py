994f0623ead0b81bdb21c30b1e66cd79921d7192
Gisbert12843 <51728041+Gisbert12843@users.noreply.github.com>
2022-04-25 14:34:48 +0200

    Update hanged man.py
    updated to a working version
    
    problems: capital letters are still accounted for seperated

--- hanged man.py
+++ hanged man.py
@@ -1,8 +1,9 @@
 from ast import Break
 
 
+
 good_drawing = [("----------"), ("|         "), ("|        "),("|        "),("|        "),("|        "),("|        "),("|        "),("|       "),("|_________")]
-bad_drawing = [("----------"), ("|      |  "), ("|      |  "),("|      O  "),("|      |  "),("|  /  |  \ "),("|     | |  "),("|     | |  "),("|     - -  "),("|_________")]
+bad_drawing = [("----------"), ("|      |  "), ("|      |  "),("|      O  "),("|      |  "),("|   /  |  \ "),("|     | |  "),("|     | |  "),("|     - -  "),("|_________")]
 
 
 
@@ -10,142 +11,65 @@ bad_drawing = [("----------"), ("|      |  "), ("|      |  "),("|      O  "),("|
 def draw(guess_count, guessed_wrong):
 
     guess_count_local = guess_count
-    guessed_wrong_local = guessed_wrong
-    for i in range(guess_count_local):
-        for i in range(guessed_wrong_local):
-            print(bad_drawing[i])
-            guess_count_local -= 1
-            i += i
-        i += i
+    guessed_wrong_local = guessed_wrong+1
+    for i in range(guessed_wrong_local):
+        print(bad_drawing[i])
+        i += 1
+    j=0
+    j+=guessed_wrong_local
+    while(j < guess_count_local):
+        print(good_drawing[j])
+        j += 1
 
-def is_in_list(char, list):
-    while(1):
-        for i in range(len(list)):
-            if(char == list[i]):
-                break
-            
-
-    return 0
+def is_in_list(char, list, list_to_be_altered=[]):
+    amount = 0
+    for i in range(len(list)):
+        if(char == list[i]):
+            if(list_to_be_altered!=[]):
+                list_to_be_altered[i]=char
+            amount+=1
+    return amount
 
 def run(secret_w):
 
-    secret_word = secret_w
-    guess_count = 10
+    secret_word = list(secret_w)
+    #guess_count = 10
     guessed_wrong = 0
-    guessed_char = ''
+    guessed_char = ""
     guessed_list = []
-    guessed_secret = ""
+    guessed_secret = [] 
+    for i in secret_word:
+        guessed_secret.append('*')
 
     print("\n\n\n\n\n\n\n***Welcome to Hang-Man!***\nIt's time for you to guess our secret word of the Day!\n\n\n\n")
     
     
 
-    while 1:
-        guessed_char = str(input("Please make a guess!\n"))
-        while(is_in_list(guessed_char)):
-            guessed_char = str(input("Letter was already guessed.\nPlease make a new guess!\n"))
+    while guessed_list != secret_word:
+        guessed_char = str(input("\nPlease make a guess!\n"))[0]
+        while(is_in_list(guessed_char, guessed_list)>0):
+            guessed_char = str(input("Letter was already guessed.\nPlease make a new guess!\n"))[0]
+        guessed_list.append(guessed_char)
 
 
-        if (is_in_list(guessed_char, secret_word)):
+        if (is_in_list(guessed_char, secret_word, guessed_secret)>0):
             print("Yes! " + guessed_char + " is part of the secret word!\n")
-            
-        draw(10, guessed_wrong)
+            print(guessed_secret)
 
 
-run("Hallo")
+        else: 
+            guessed_wrong+=1
+            draw(10, guessed_wrong)
+        
+        if(guessed_wrong>=8):
+            print("Exceeded 10 guesses :( guess you suck..\n")
+            return
+        elif(guessed_secret == secret_word):
+            break
+    print("\n\n-----------------------------------------------\n|\t\t\t\t\t\t|\n|\t\t\t\t\t\t|")
+    print("|\t***OMG YOU DID IT YOU MADLAD!!!***\t|")
+    print("|\t\t\t\t\t\t|\n|\t\t\t\t\t\t|\n----------------------------------------------\n\n")
 
 
-print("***OMG YOU DID IT YOU MADLAD!!!***")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-###THIS IS THE SHIT VERSION
-
-"""
-good_drawing = [("----------"), ("|      |  "), ("|      |  "),("|      O  "),("|      |  "),("|    /   \ "),("|      |  "),("|     | |  "),("|     - -  "),("|_________")]
-
-def draw(x):
-    for i in range(x):
-        print(good_drawing[i])
-        i += i
-
-
-
-def run(secret_w):
-    guess_count = 0
-    guessed_word = "NONE"
-    secret_word = secret_w
-    print("\n\n\n\n\n\n\n***Welcome to Hang-Man!***\nIt's time for you to guess our secret word of the Day!\n\n\n\n")
-    guessed_word = str(input("Please make a guess!\n"))
-    while guessed_word != secret_word:
-        guess_count += 1
-        draw(guess_count)
-        guessed_word = str(input("Please make a guess!\n"))
-
-
-run("Hallo")
-print("***OMG YOU DID IT YOU MADLAD!!!***")
-"""
+run("mama")
